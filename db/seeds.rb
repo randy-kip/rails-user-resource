bee3ca1bf930166973c1d81f68306c466bc67f6e
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-12-18 18:17:00 +0300

    feat: Add seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# db/seeds.rb
+
+# Clear existing records
+puts "Destroying existing records..."
+Company.destroy_all
+StockPrice.destroy_all
+
+# Seed data
+puts "Seeding data..."
+
+# Create some companies
+Company.create(name: 'TechCorp', ticker_symbol: 'TC')
+Company.create(name: 'FinanceCo', ticker_symbol: 'FC')
+Company.create(name: 'HealthCareInc', ticker_symbol: 'HCI')
+
+# Create stock prices for each company
+techcorp = Company.find_by(ticker_symbol: 'TC')
+financeco = Company.find_by(ticker_symbol: 'FC')
+healthcareinc = Company.find_by(ticker_symbol: 'HCI')
+
+# Create stock prices for each company
+StockPrice.create(price: 150.75, captured_at: Date.today, company: techcorp)
+StockPrice.create(price: 65.20, captured_at: Date.today, company: financeco)
+StockPrice.create(price: 120.50, captured_at: Date.today, company: healthcareinc)
+
+puts "Seed data successfully created!"
