26ef57d0575b9a0f16767e8646c7d409a0941d8e
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-12-18 18:34:36 +0300

    feat: Set up validation in the Company model.

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,18 @@
 class Company < ApplicationRecord
     has_many :stock_prices, dependent: :destroy
+
+    validates :name, presence: true, uniqueness: true
+    validates :ticker_symbol, presence: true, uniqueness: true
+
+    validate :validate_length_of_ticker_symbol
+
+    private 
+    
+    def validate_length_of_ticker_symbol
+        if self.ticker_symbol.present?
+            if self.ticker_symbol.size < 2 or self.ticker_symbol.size > 4
+                self.errors.add(:ticker_symbol, "Length should be at least 2 and at most 4 in length.")
+            end
+        end
+    end
 end
